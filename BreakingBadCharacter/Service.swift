b9a9c2bb1a03900b2e88816ca3bb59f79a027ae0
zip520123 <zip520123@yahoo.com.tw>
2021-02-21 22:25:32 +0000

    Add localService

--- BreakingBadCharacter/Service.swift
+++ BreakingBadCharacter/Service.swift
@@ -9,3 +9,22 @@ import Foundation
 protocol Service {
     func loadData(completionHandler: @escaping ([MovieCharacter], Error?) -> Void)
 }
+
+struct LocalService: Service {
+    
+    enum ServiceError: Error {
+        case readFileFail
+    }
+    
+    func loadData(completionHandler: @escaping ([MovieCharacter], Error?) -> Void) {
+        guard let path = Bundle.main.url(forResource: "Characters.json", withExtension: nil),
+              let data = try? Data(contentsOf: path),
+              let model = try? JSONDecoder().decode(MovieCharacters.self, from: data)
+              else {
+            completionHandler([], ServiceError.readFileFail)
+            return
+        }
+        completionHandler(model,nil)
+        
+    }
+}
