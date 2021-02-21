1ede42230e0be54d37f39461935c1e85d2863160
zip520123 <zip520123@yahoo.com.tw>
2021-02-21 18:53:58 +0000

    Move makeACharacter for reuse.

--- BreakingBadCharacterTests/CharactersViewControllerTests.swift
+++ BreakingBadCharacterTests/CharactersViewControllerTests.swift
@@ -19,7 +19,7 @@ class CharactersViewControllerTests: XCTestCase {
     
     func test_viewDidLoad_renderOneCharacterName() {
         let sut = makeSUT([makeACharacter()])
-        XCTAssertEqual(sut.tableView.title(at: 0), "Walter White")
+        XCTAssertEqual((sut.tableView.cell(at: 0) as? CharacterCell)?.nameLabel.text, "Walter White")
     }
 
     func makeSUT(_ characters: MovieCharacters = []) -> CharactersViewController {
@@ -28,40 +28,21 @@ class CharactersViewControllerTests: XCTestCase {
         return sut
     }
     
-    func makeACharacter() -> MovieCharacter {
-        MovieCharacter(
-            charID: 1,
-            name: "Walter White",
-            birthday: BreakingBadCharacter.Birthday.the09071958,
-            occupation: ["High School Chemistry Teacher", "Meth King Pin"],
-            img: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
-            status: BreakingBadCharacter.Status.presumedDead,
-            nickname: "Heisenberg",
-            appearance: [1, 2, 3, 4, 5],
-            portrayed: "Bryan Cranston",
-            category: BreakingBadCharacter.Category.breakingBad,
-            betterCallSaulAppearance: [])
-    }
+
 }
 
-extension UITableView {
-    func cell(at row: Int) -> UITableViewCell? {
-        return dataSource?.tableView(self, cellForRowAt: IndexPath(row: row, section: 0))
-    }
-    
-    func title(at row: Int) -> String? {
-        return self.cell(at: row)?.textLabel?.text
-    }
-    
-    func select(at row: Int) {
-        let indexPath = IndexPath(row: row, section: 0)
-        selectRow(at: indexPath, animated: false, scrollPosition: .none)
-        delegate?.tableView?(self, didSelectRowAt: indexPath)
-    }
-    
-    func deselect(at row: Int) {
-        let indexPath = IndexPath(row: row, section: 0)
-        deselectRow(at: indexPath, animated: false)
-        delegate?.tableView?(self, didDeselectRowAt: indexPath)
-    }
+func makeACharacter() -> MovieCharacter {
+    MovieCharacter(
+        charID: 1,
+        name: "Walter White",
+        birthday: BreakingBadCharacter.Birthday.the09071958,
+        occupation: ["High School Chemistry Teacher", "Meth King Pin"],
+        img: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
+        status: BreakingBadCharacter.Status.presumedDead,
+        nickname: "Heisenberg",
+        appearance: [1, 2, 3, 4, 5],
+        portrayed: "Bryan Cranston",
+        category: BreakingBadCharacter.Category.breakingBad,
+        betterCallSaulAppearance: [])
 }
+
