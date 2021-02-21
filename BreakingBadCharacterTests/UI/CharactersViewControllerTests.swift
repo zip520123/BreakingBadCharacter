cf051a0ab1764a28a445471f4f0a532f963fd20f
zip520123 <zip520123@yahoo.com.tw>
2021-02-21 23:32:08 +0000

    Move makeACharacter to its own file

--- BreakingBadCharacterTests/UI/CharactersViewControllerTests.swift
+++ BreakingBadCharacterTests/UI/CharactersViewControllerTests.swift
@@ -50,18 +50,4 @@ class CharactersViewControllerTests: XCTestCase {
 
 }
 
-func makeACharacter() -> MovieCharacter {
-    MovieCharacter(
-        charID: 1,
-        name: "Walter White",
-        birthday: BreakingBadCharacter.Birthday.the09071958,
-        occupation: ["High School Chemistry Teacher", "Meth King Pin"],
-        img: "https://images.amcnetworks.com/amc.com/wp-content/uploads/2015/04/cast_bb_700x1000_walter-white-lg.jpg",
-        status: BreakingBadCharacter.Status.presumedDead,
-        nickname: "Heisenberg",
-        appearance: [1, 2, 3, 4, 5],
-        portrayed: "Bryan Cranston",
-        category: BreakingBadCharacter.Category.breakingBad,
-        betterCallSaulAppearance: [])
-}
 
