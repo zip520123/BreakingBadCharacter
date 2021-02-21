7b678437c6e34842957563685644059696ade4f6
zip520123 <zip520123@yahoo.com.tw>
2021-02-21 18:54:31 +0000

    Add tableview helper

---
+++ BreakingBadCharacterTests/helper/TableViewHelper.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewHelper.swift
+//  BreakingBadCharacterTests
+//
+//  Created by zip520123 on 21/02/2021.
+//
+
+import UIKit
+extension UITableView {
+    func cell(at row: Int) -> UITableViewCell? {
+        return dataSource?.tableView(self, cellForRowAt: IndexPath(row: row, section: 0))
+    }
+    
+    func title(at row: Int) -> String? {
+        return self.cell(at: row)?.textLabel?.text
+    }
+    
+    func select(at row: Int) {
+        let indexPath = IndexPath(row: row, section: 0)
+        selectRow(at: indexPath, animated: false, scrollPosition: .none)
+        delegate?.tableView?(self, didSelectRowAt: indexPath)
+    }
+    
+    func deselect(at row: Int) {
+        let indexPath = IndexPath(row: row, section: 0)
+        deselectRow(at: indexPath, animated: false)
+        delegate?.tableView?(self, didDeselectRowAt: indexPath)
+    }
+}
