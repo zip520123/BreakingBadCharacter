f16f8d1ca948a34483b21ca1697dca94cd93244a
zip520123 <zip520123@yahoo.com.tw>
2021-02-22 10:40:45 +0000

    Add api service

--- BreakingBadCharacter/Service.swift
+++ BreakingBadCharacter/Service.swift
@@ -6,15 +6,61 @@
 //
 
 import Foundation
+
+enum ServiceError: Error {
+    case urlIsNil
+    case readFileFail
+    case decodeError
+}
+
 protocol Service {
     func loadData(completionHandler: @escaping ([MovieCharacter], Error?) -> Void)
 }
 
-struct LocalService: Service {
+struct ApiService: Service {
+    public let session: URLSession
+    public let scheme = "https"
+    public let host = "breakingbadapi.com"
     
-    enum ServiceError: Error {
-        case readFileFail
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
     }
+    
+    private func defualtURLComponets() -> URLComponents {
+        var urlc = URLComponents()
+        urlc.host = host
+        urlc.scheme = scheme
+        return urlc
+    }
+    
+    func loadData(completionHandler: @escaping ([MovieCharacter], Error?) -> Void) {
+        var urlc = defualtURLComponets()
+        urlc.path = "/api/characters"
+        
+        guard let url = urlc.url else {
+            completionHandler([], ServiceError.urlIsNil)
+            return
+        }
+        print(url)
+        let request = URLRequest(url: url)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completionHandler([], error)
+                return
+            }
+            guard let data = data, let model = try? JSONDecoder().decode(MovieCharacters.self, from: data) else {
+                completionHandler([], ServiceError.decodeError)
+                return
+            }
+            completionHandler(model, nil)
+        }
+        
+        task.resume()
+    }
+}
+
+struct LocalService: Service {
     
     func loadData(completionHandler: @escaping ([MovieCharacter], Error?) -> Void) {
         guard let path = Bundle.main.url(forResource: "Characters.json", withExtension: nil),
