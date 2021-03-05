d962ddc0da19a5a03e48399bb7aec2436a1242b2
zip520123 <zip520123@yahoo.com.tw>
2021-03-05 15:50:26 +0000

    Remove AppFlow business tests.

--- BreakingBadCharacterTests/FlowTests.swift
+++ BreakingBadCharacterTests/FlowTests.swift
@@ -40,57 +40,6 @@ class FlowTests: XCTestCase {
         XCTAssertNotEqual(sut.currAllCharacters.value.count, 0)
     }
     
-    func test_charactersSearchEmptyTest_getAllCharacters() {
-        let viewModel = makeViewModel()
-        
-        let sut = makeSUT(service: LocalService(), viewModel: viewModel)
-        
-        let allCharactersCount = sut.currAllCharacters.value.count
-        viewModel.searchText.accept("")
-        
-        XCTAssertEqual(sut.currAllCharacters.value.count, allCharactersCount)
-    }
-    
-    func test_charactersSearchSomeText_getAllCharactersWhosNameContainsText() {
-        let viewModel = makeViewModel()
-
-        let sut = makeSUT(service: LocalService(), viewModel: viewModel)
-        viewModel.searchText.accept("Walter")
-        
-        for character in sut.currAllCharacters.value {
-            XCTAssertTrue(character.name.contains("Walter"))
-        }
-
-    }
-    
-    func test_charactersSearchSomeTextAndSearchEmpty_getAllCharacters() {
-        let viewModel = makeViewModel()
-        
-        let sut = makeSUT(service: LocalService(), viewModel: viewModel)
-        
-        let allCharactersCount = sut.currAllCharacters.value.count
-        viewModel.searchText.accept("Walter")
-        viewModel.searchText.accept("")
-        
-        XCTAssertEqual(sut.currAllCharacters.value.count, allCharactersCount)
-    }
-    
-    func test_searchSomeTextAndSelectSeasonAppearance_getCharactersWhosNameContainsTextWithSeasonAppearance() {
-        let viewModel = makeViewModel()
-        let sut = makeSUT(service: LocalService(), viewModel: viewModel)
-        viewModel.searchText.accept("Jane")
-        viewModel.seasionAppearance.accept(1)
-        XCTAssertEqual(sut.currAllCharacters.value.count, 0)
-    }
-    
-    func test_searchSomeTextAndSelectAllSeasonAppearance() {
-        let viewModel = makeViewModel()
-        let sut = makeSUT(service: LocalService(), viewModel: viewModel)
-        viewModel.searchText.accept("Jane")
-        viewModel.seasionAppearance.accept(0)
-        XCTAssertEqual(sut.currAllCharacters.value.count, 1)
-    }
-    
     func makeSUT(service: Service = MockService(), viewModel: CharactersViewModel = makeViewModel()) -> AppFlow {
         let sut = AppFlow(charactersViewModel: viewModel)
         sut.start()
