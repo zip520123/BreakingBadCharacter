38c3adc09b6ee3b9963261c8ace58d6f1a72cea0
zip520123 <zip520123@yahoo.com.tw>
2021-03-05 14:06:56 +0000

    Add currAllCharacters to CharactersViewModel

--- BreakingBadCharacter/CharactersViewModel.swift
+++ BreakingBadCharacter/CharactersViewModel.swift
@@ -12,4 +12,5 @@ struct CharactersViewModel {
     let searchText: BehaviorRelay<String> = BehaviorRelay<String>(value: "")
     let didSelectCharacter: PublishRelay<MovieCharacter> = PublishRelay<MovieCharacter>()
     let currFilteredCharacters: BehaviorRelay<[MovieCharacter]> = BehaviorRelay<[MovieCharacter]>(value: [])
+    let currAllCharacters: BehaviorRelay<[MovieCharacter]> = BehaviorRelay<[MovieCharacter]>(value: [])
 }
